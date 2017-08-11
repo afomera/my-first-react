483a13d0116109e693f34a91ac1eb96a860272cf
Andrew Fomera <andrew@zerlex.net>
2017-08-11 09:04:54 -0500

    Initial commit going through tutorials

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld name="Andrew"/>
+      <HelloWorld name="Scott"/>
+      <HelloWorld name="Kyle"/>
+    </div>
+  );
+}
+
+export default App;
